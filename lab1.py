11473781ba13dad7fbfdac05a7988dca06c4d516
angryhero-face <86159182+angryhero-face@users.noreply.github.com>
2021-06-19 19:27:23 +0300

    Delete lab1.py

--- lab1.py
+++
@@ -1,8 +0,0 @@
-number1 = int(input("введите число: "))
-number2 = 8
-if number1 < number2:
-    print("1ое число меньше пограничного")
-if number1 > number2:
-    print("1ое число больше пограничного")
-if number1 > number2*3:
-    print("1ое число больше пограничного более, чем в 3 раза")
