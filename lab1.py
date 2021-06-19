eec801c1a3214b120c2a0fe2c81b90eaa1859fd1
angryhero-face <86159182+angryhero-face@users.noreply.github.com>
2021-06-19 19:11:02 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,8 @@
+number1 = int(input("введите число: "))
+number2 = 8
+if number1 < number2:
+    print("1ое число меньше пограничного")
+if number1 > number2:
+    print("1ое число больше пограничного")
+if number1 > number2*3:
+    print("1ое число больше пограничного более, чем в 3 раза")
